3a22776761ea2a0d93ba37b6ea3ec0bb1b8997ba
anji <anji_cruz@msn.com>
2024-02-15 12:40:47 +0800

    cars.py 2/15

---
+++ cars.py
@@ -0,0 +1,13 @@
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+len(cars)
+# cars.sort()
+# cars.sort(reverse=True)
+# print(cars)
+
+# print("\nHere is the sorted list:")
+# print(sorted(cars))
+
+# print("\nHere is the original sorted list again:")
+# print(cars)
+# cars.reverse()
+# print(cars)
